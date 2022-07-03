225015191a32d8f61ae487cf07d66a98e0b153da
Nurmuhammad-Ikromov <Nurmuhammadikromov58@gmail.com>
2022-07-03 23:51:28 +0500

    add extra changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var elList = document.querySelector('.js-list');
-
-let res = [];
+var elSelect = document.querySelector('.js-select');
+let topl = [];
 
 for (item of films) {
 	var newItem = document.createElement('li');
@@ -9,7 +9,7 @@ for (item of films) {
 	var textItem = document.createElement('p');
 	var time = document.createElement('time');
 	var img = document.createElement('img');
-    var box = document.createElement('div')
+	var box = document.createElement('div');
 	img.src = `${item.poster}`;
 
 	newItem.setAttribute('class', 'js-item');
@@ -20,7 +20,7 @@ for (item of films) {
 	img.setAttribute('class', 'js-img');
 
 	var date = new Date(item.release_date * 1000);
-	time.textContent ="Release year: " + date.getFullYear();
+	time.textContent = 'Release year: ' + date.getFullYear();
 
 	nameItem.textContent = `${item.title}`;
 	idItem.textContent = `Film's id: ${item.id}`;
@@ -31,32 +31,100 @@ for (item of films) {
 	box.appendChild(nameItem);
 	box.appendChild(time);
 	box.appendChild(textItem);
-    newItem.appendChild(box)
+	newItem.appendChild(box);
 
-    var genBox =document.createElement('div')
-    var genText = document.createElement('span')
-    genText.textContent = "Genres:"
-    genBox.appendChild(genText)
-    genBox.setAttribute('class', 'js-genbox')
-    genText.setAttribute('class', 'js-genText')
-    box.setAttribute('class', 'js-box')
-
+	var genBox = document.createElement('div');
+	var genText = document.createElement('span');
+	genText.textContent = 'Genres:';
+	genBox.appendChild(genText);
+	genBox.setAttribute('class', 'js-genbox');
+	genText.setAttribute('class', 'js-genText');
+	box.setAttribute('class', 'js-box');
 
 	for (let i = 0; i < item.genres.length; i++) {
 		if (item.genres[i] != null) {
-			res.push(item.genres[i]);
-			var genr = document.createElement('span');
-			genr.textContent = res[i];
-			genr.setAttribute('class', 'js-genre');
-			genBox.appendChild(genr);
-
+			topl.push(item.genres[i]);
 		}
 	}
-            box.appendChild(genBox)
 
-
+	var genr = document.createElement('span');
+	genr.textContent = item.genres;
+	genr.setAttribute('class', 'js-genre');
+	genBox.appendChild(genr);
+	box.appendChild(genBox);
 
 	elList.appendChild(newItem);
 }
 
-// console.log(date.getFullYear());
+const toplam = [...new Set(topl)];
+
+for (let i = 0; i < toplam.length; i++) {
+	var newOption = document.createElement('option');
+	newOption.value = toplam[i];
+	newOption.textContent = toplam[i];
+	newOption.classList.add('js-option');
+	elSelect.appendChild(newOption);
+}
+
+let result = [];
+
+elSelect.addEventListener('change', function (evt) {
+	evt.preventDefault();
+	result = [];
+	elList.innerHTML = '';
+	let val = elSelect.value;
+	films.forEach((element) => {
+		if (element.genres.includes(val)) {
+			result.push(element);
+		}
+	});
+
+	for (item of result) {
+		var newItem = document.createElement('li');
+		var nameItem = document.createElement('p');
+		var idItem = document.createElement('span');
+		var textItem = document.createElement('p');
+		var time = document.createElement('time');
+		var img = document.createElement('img');
+		var box = document.createElement('div');
+		img.src = `${item.poster}`;
+
+		newItem.setAttribute('class', 'js-item');
+		idItem.setAttribute('class', 'js-id');
+		nameItem.setAttribute('class', 'js-item-name');
+		textItem.setAttribute('class', 'js-item-text');
+		time.setAttribute('class', 'js-time');
+		img.setAttribute('class', 'js-img');
+
+		var date = new Date(item.release_date * 1000);
+		time.textContent = 'Release year: ' + date.getFullYear();
+
+		nameItem.textContent = `${item.title}`;
+		idItem.textContent = `Film's id: ${item.id}`;
+		textItem.textContent = `About the movie: ${item.overview}`;
+
+		newItem.appendChild(img);
+		box.appendChild(idItem);
+		box.appendChild(nameItem);
+		box.appendChild(time);
+		box.appendChild(textItem);
+		newItem.appendChild(box);
+
+		var genBox = document.createElement('div');
+		var genText = document.createElement('span');
+		genText.textContent = 'Genres:';
+		genBox.appendChild(genText);
+		genBox.setAttribute('class', 'js-genbox');
+		genText.setAttribute('class', 'js-genText');
+		box.setAttribute('class', 'js-box');
+
+		box.appendChild(genBox);
+
+		var genr = document.createElement('span');
+		genr.textContent = item.genres;
+		genr.setAttribute('class', 'js-genre');
+		genBox.appendChild(genr);
+		box.appendChild(genBox);
+		elList.appendChild(newItem);
+	}
+});
